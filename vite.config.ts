4dd8b0105e78fe86b0bb83e83debb062ff8f5096
RePlayZz9 <134027594+RePlayZz9@users.noreply.github.com>
2025-06-30 10:10:41 +0500

    Update deploy.yml and vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/izotrub_abc/' : '/',
+  base: "./",
   css: {
     postcss: {
       plugins: [tailwind()],
