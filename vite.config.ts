d7b159b88347c2a9ead9e12296b829990b8ce3b4
RePlayZz9 <134027594+RePlayZz9@users.noreply.github.com>
2025-06-30 09:35:39 +0500

    `Update vite.config.ts to conditionally set base path based on environment`

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/izotrub/abc/' : '/',
   css: {
     postcss: {
       plugins: [tailwind()],
