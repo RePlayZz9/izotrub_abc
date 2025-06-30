6e6e616e9ca10f228ad01bb3c8689a76a50f4dc4
RePlayZz9 <134027594+RePlayZz9@users.noreply.github.com>
2025-06-30 10:07:23 +0500

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/izotrub/abc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/izotrub_abc/' : '/',
   css: {
     postcss: {
       plugins: [tailwind()],
